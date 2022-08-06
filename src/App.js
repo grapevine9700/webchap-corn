c42f94c41c571c25e55bc0cca15f432ebe0e415c
grapevine9700 <99169493+grapevine9700@users.noreply.github.com>
2022-08-06 18:48:06 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+const GundamHeader = () => {
+  return <h3>찰옥수수 </h3>;
+};
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <GundamHeader />
+    </div>
+  );
+}
